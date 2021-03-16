8705047a8b95dca84ebbdbb63a5c522b53398d50
unknown <kongm1@yahoo.com>
2021-03-16 11:37:22 -0400

    update version

--- tools/generators/sort-project-references/index.ts
+++ tools/generators/sort-project-references/index.ts
@@ -1,10 +1,14 @@
-import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
+import {Tree, formatFiles, installPackagesTask, updateJson} from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
 export default async function (host: Tree, schema: any) {
-  await libraryGenerator(host, { name: schema.name });
-  await formatFiles(host);
-  return () => {
-    installPackagesTask(host);
-  };
+  updateJson(host, 'workspace.json', json => {
+    json.version++;
+    return json;
+  });
+  // await libraryGenerator(host, { name: schema.name });
+  // await formatFiles(host);
+  // return () => {
+  //   installPackagesTask(host);
+  // };
 }
